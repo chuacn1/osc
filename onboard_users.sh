d983000ecdfce0b7618a993172f81e59f172c563
chuacn1 <chuacn1@student.op.ac.nz>
2025-08-04 23:34:58 +1200

    help meee

--- onboard_users.sh
+++ onboard_users.sh
@@ -84,8 +84,10 @@ if grep -q "^$username," users.csv; then
         read -p "Enter new shell [$current_shell]: " new_shell
         new_shell=${new_shell:-$current_shell}
     
-# This will skip the header and only modify matching user rows
-        sed -i "1!{/^$username,/ s/\([^,]*,[^,]*,\)[^,]*/\1$new_shell/}" users.csv
+        awk -F',' -v user="$username" -v new_shell="$new_shell" '
+        BEGIN {OFS=","} 
+        NR==1 || $1 != user {print} 
+        $1 == user {$3=new_shell; print}' users.csv
         log_action "Updated CSV: $username now has shell $new_shell"
         listUsers
     fi
